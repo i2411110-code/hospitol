4da997023f66fde9ccecee1748185664a5284395
i2411110-code <i2411110@tossinsu-advisor.com>
2026-07-24 13:21:39 +0900

    수정

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,11 @@
-// Firebase 콘솔 > 프로젝트 설정 > 일반 > "내 앱" 에서 복사한 값을 아래에 그대로 붙여넣으세요.
-// 이 값들은 공개되어도 문제 없습니다 (Firebase 웹 앱의 정상적인 동작 방식).
-// 실제 보안은 firestore.rules 파일로 서버 단에서 강제됩니다.
-
+// ⚠️ 2026-07-24: 보험가온포탈(toinsu)과 계정을 공유하기 위해
+// 이 앱 전용 프로젝트(hospitol-db027)에서 공용 프로젝트(gaon-f3399)로 전환했습니다.
 export const firebaseConfig = {
-  apiKey: "AIzaSyBA2JcsDgrNIc14qx9jPPExs9tNrc9nzyA",
-  authDomain: "hospitol-db027.firebaseapp.com",
-  projectId: "hospitol-db027",
-  storageBucket: "hospitol-db027.firebasestorage.app",
-  messagingSenderId: "623672620156",
-  appId: "1:623672620156:web:ca66faf3869328796205f9"
+  apiKey: "AIzaSyA1GU9E5WuFUPIyT4Ma4_crArdANpCMQfs",
+  authDomain: "gaon-f3399.firebaseapp.com",
+  projectId: "gaon-f3399",
+  storageBucket: "gaon-f3399.firebasestorage.app",
+  messagingSenderId: "870111782359",
+  appId: "1:870111782359:web:d6d3eeb441d1ac06169792",
+  measurementId: "G-JCW1K0YNZ3"
 };
